f137f2d68424b4fd167c595eaa864799046b134c
devjuninho19 <fareisorrir@gmail.com>
2024-09-21 21:24:53 -0300

    modifiquei o caminho na importação do arquivo de rotas adicionando o js a qual estava dando pproblema

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv'; // Importar dotenv
-
+import router from './Routes/Router.js';
 dotenv.config(); // Carregar variáveis de ambiente do arquivo .env
 
 const port = process.env.PORT || 5000; // Usa 5000 se process.env.PORT não estiver definido
 
 const app = express();
+//routes
 
+app.use(router)
 // Configurar JSON e resposta de dados de formulário
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
