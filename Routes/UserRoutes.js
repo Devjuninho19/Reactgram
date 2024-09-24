dd692476369dd638572367a21398910ffbaff0b7
devjuninho19 <fareisorrir@gmail.com>
2024-09-24 20:20:13 -0300

    controllers do usuário e middlewares das aulas 191 e 192

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 // Controller
 import { register } from "../controllers/UserController.js"; // Use a sintaxe de importação ES6
-
+//Middlewares
+import validate from "../middlewares/handleValidation.js";
 // Routes
-router.post("/register", register);
+router.post("/register",validate, register);
 
 export default router; // Use export default em vez de module.exports
